c5194ae1128d0adaae689c97a04a496d63039793
XtinaT <90282185+XtinaT@users.noreply.github.com>
2021-09-24 14:07:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+/*let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+function multiply(num1,num2){
+	result = num1*num2;
+	console.log(result);
+}
+multiply(4,7);
+multiply(20,20);
+document.querySelector("html").onclick = function(){
+	alert("Ouch! Stop poking me!");
+}*/
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+let mySrc=myImage.getAttribute("src");
+if (mySrc==="images/IMG_7592.JPG"){
+	myImage.setAttribute ("src","images/IMG_8142.JPG");
+}else{
+	myImage.setAttribute("src","images/IMG_7592.JPG");
+}
+}
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName(){
+	let userName = prompt("Please enter your name.");
+	if(!userName){
+		setUserName();
+	}else{
+		localStorage.setItem("name",userName);
+		myHeading.textContent = "Hello, "+userName;
+	}
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName;
+}
+myButton.onclick = function(){
+	setUserName();
+}
