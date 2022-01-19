4e10cd969020e0483cf2736d1c69321b7bf5b28f
XtinaT <tomasheva_xtina@icloud.com>
2022-01-19 21:34:52 +0300

    first

---
+++ showList.js
@@ -0,0 +1,10 @@
+var isShown="No";
+function showList() {
+  if (isShown=="No") {
+  hidden_list.style.display="inline-block";
+  isShown="Yes";
+  } else {
+   hidden_list.style.display="none"; 
+   isShown="No";
+  } 
+}
